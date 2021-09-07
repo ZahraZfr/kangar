752434b084288e01d46dc0c00a33f5ee84faab96
ZahraZfr <Zahra.Z@m.lo.agency>
2021-09-08 00:06:26 +0430

    feat:js of location section and font correction

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
+// --------------for scrolling verticaly 
 window.addEventListener('scroll', function () {
-    let header = document.querySelector('header');
-    let scrollSec = document.getElementsByClassName('scrolly-nav')[0];
-    let windowPosition = window.scrollY > 0;
-    header.classList.toggle('scrolling-active', windowPosition);
-    scrollSec.classList.toggle('scrolly-active', windowPosition);
+  let header = document.querySelector('header');
+  let scrollSec = document.getElementsByClassName('scrolly-nav')[0];
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle('scrolling-active', windowPosition);
+  scrollSec.classList.toggle('scrolly-short', windowPosition);
 });
+
+// ----------------for opening dropdowns
+const userdd = document.querySelector("#userDropDown");
+const userddBTN = document.querySelector("#user-menu-right");
+
+function userMenuDropdown() {
+  userdd.classList.toggle("show");
+  console.log('open dropdown');
+}
+
+
+// --------------------location dropdown
+const locationn = document.querySelector(".locationDD");
+const locationBTN = document.querySelector(".location");
+const checkinn = document.querySelector(".checkin");
+function showfunction() {
+  locationn.classList.toggle("showLocation");
+  locationBTN.classList.toggle("showLocationBTN");
+  checkinn.classList.toggle("checkinBRD")
+  console.log("open dropdown for location");
+}
+
+// ------for Close the dropdown if the user clicks outside of it
+// document.onclick = function (e) {
+//   console.log("outer close dropdown if");
+//   if (e.target.id !== 'userdd' && e.target.id !== 'userddBTN') {
+//     userdd.classList.remove("show");
+//     console.log("inner close dropdown if");
+//   }
+// }
+
+// $(document).click(function (event) {
+//   if (!$(event.target).closest(userdd).length) {
+//     userdd.style.display = "none";
+//     console.log("aaaaaaa");
+//   }
+// });
 
 
 
