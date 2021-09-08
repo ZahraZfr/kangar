55ff201de994349ea036760202f2b3efe49dea79
ZahraZfr <Zahra.Z@m.lo.agency>
2021-09-09 00:24:51 +0430

    feat:copmlete html and css js for guests section when it drops down and close

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ const checkinn = document.querySelector(".checkin");
 const checkoutt = document.querySelector(".checkout");
 const guestss = document.querySelector(".guests");
 const guestsBTN = document.querySelector(".guests");
-const pinkSearch = document.querySelector(".serach-icon")
+const guedtsDD = document.querySelector(".guestsDD");
+const pinkSearch = document.querySelector(".serach-icon");
 // --------------------location dropdown
 function showLocationF() {
   locationn.classList.toggle("showLocation");
@@ -35,20 +36,36 @@ function showLocationF() {
   pinkSearch.classList.toggle("serachShow");
   console.log("open dropdown for location");
 }
-
+// ---------------------guests dropdown
+function showGuestsF() {
+  guestsBTN.classList.toggle("showGuestBTN");
+  pinkSearch.classList.toggle("serachShow");
+  guedtsDD.classList.toggle("guedtsDDshow");
+  console.log("open dropdown for guests");
+}
 
 // --------closer dropdown of signin btn
 $(document).click(function (event) {
+  // for signin
   if ((!$(event.target).closest(userdd).length) && (!$(event.target).closest(userddBTN).length)) {
     userdd.classList.remove("show")
     console.log("close drop down");
   }
+  // for location
   if ((!$(event.target).closest(locationn).length) && (!$(event.target).closest(locationBTN).length)) {
     locationn.classList.remove("showLocation");
     locationBTN.classList.remove("showLocationBTN");
     checkinn.classList.remove("checkinSEC");
+    checkoutt.classList.remove("checkoutSEC");
     pinkSearch.classList.remove("serachShow");
     console.log("close drop down location");
+  }
+  // for guests
+  if ((!$(event.target).closest(guestsBTN).length) && (!$(event.target).closest(guestsBTN).length)) {
+    guestsBTN.classList.remove("showGuestBTN");
+    pinkSearch.classList.remove("serachShow");
+    guedtsDD.classList.remove("guedtsDDshow");
+    console.log("close drop down guests");
   }
 });
 
