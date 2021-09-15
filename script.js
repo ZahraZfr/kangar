38c50b2813cfbbad1eeb3bc6c72845e30e3a6ec8
ZahraZfr <Zahra.Z@m.lo.agency>
2021-09-15 20:17:21 +0430

    feat:complete experience tab and function for places to stay link

--- script.js
+++ script.js
@@ -5,32 +5,49 @@ window.addEventListener('scroll', function () {
   let windowPosition = window.scrollY > 0;
   header.classList.toggle('scrolling-active', windowPosition);
   scrollSec.classList.toggle('scrolly-short', windowPosition);
-  guestsBTN.classList.remove("showGuestBTN" , windowPosition);
-  pinkSearch.classList.remove("serachShowG", windowPosition);
-  guedtsDD.classList.remove("guedtsDDshow" , windowPosition);
-  pinkSearch.classList.remove("serachShow", windowPosition);
+  guestsBTN.classList.remove("showGuestBTN", windowPosition);
+  pinkSearch.classList.remove("searchShowG", windowPosition);
+  pinkSearch.classList.remove("searchShow", windowPosition);
+  guedtsDD.classList.remove("guedtsDDshow", windowPosition);
 });
+// back to place to stay
+const placesToStay = document.querySelector(".nav-list1");
+let scrollyNPlaces = document.getElementsByClassName('scrolly-nav')[0];
+
+function firstTab() {
+  scrollExp.classList.remove("showExperience");
+}
+// experience tab
+const experienceTab = document.querySelector('.nav-list2');
+const ExperienceTab1 = document.querySelector('.ExperienceTab1');
+const ExperienceTab2 = document.querySelector('.ExperienceTab2');
+const DateDD = document.querySelector("#LocDropdown");
+const scrollExp = document.getElementsByClassName('scrolly-nav')[0];
+const pinkSearchDD = document.querySelector(".search-DD");
+// experience tab
+function showExperience() {
+  scrollExp.classList.add("showExperience");
+}
+// experience date tab
+function showDataExp() {
+  ExperienceTab2.classList.add("showDateBTN");
+  pinkSearchDD.classList.add("searchShowDDD");
+}
+// experience location tab
+function showLocExp() {
+  DateDD.classList.add("activeFlex");
+  ExperienceTab1.classList.add("showLocationBTN");
+  pinkSearchDD.classList.add("searchShowDD");
+}
+
 // underline for navbar item
 const navLi1 = document.querySelector(".nav-list1");
 const navUL = document.querySelector(".nav-list");
-
-// navListItem.forEach((item) => {
-//     item.addEventListener("click", () => {
-//         item.classList.toggle("underline")
-//     })
-// });
-
-function openItem(event){
-
+function openItem(event) {
   tabItem = document.getElementsByClassName("nav-list-i");
-  // let tabUnderline = document.getElementsByClassName("tab-content");
-
-  // for (var i = 0; i < tabcontent.length; i++) {
-  //   tabcontent[i].style.display = "none";
-  // }
   for (i = 0; i < tabItem.length; i++) {
     tabItem[i].className = tabItem[i].className.replace(" ShowUnderline", "");
-    
+
   }
 
   event.currentTarget.className += " ShowUnderline";
@@ -55,7 +72,7 @@ const checkoutt = document.querySelector(".checkout");
 const guestss = document.querySelector(".guests");
 const guestsBTN = document.querySelector(".guests");
 const guedtsDD = document.querySelector(".guestsDD");
-const pinkSearch = document.querySelector(".serach-icon");
+const pinkSearch = document.querySelector(".search-icon");
 // --------------------location dropdown
 function showLocationF() {
   locationn.classList.add("showLocation");
@@ -63,12 +80,12 @@ function showLocationF() {
   checkinn.classList.add("checkinSEC");
   checkoutt.classList.add("checkoutSEC");
   guestss.classList.add("guestsSEC");
-  pinkSearch.classList.add("serachShow");
+  pinkSearch.classList.add("searchShow");
 }
 // ---------------------guests dropdown
 function showGuestsF() {
   guestsBTN.classList.add("showGuestBTN");
-  pinkSearch.classList.add("serachShowG");
+  pinkSearch.classList.add("searchShowG");
   guedtsDD.classList.add("guedtsDDshow");
   checkoutt.classList.add("checkoutSECBRD");
   checkinn.classList.add("checkinG");
@@ -78,6 +95,17 @@ function showGuestsF() {
 
 // --------closer dropdown of signin btn
 $(document).click(function (event) {
+  // for location experience
+  if ((!$(event.target).closest(DateDD).length) && (!$(event.target).closest(ExperienceTab1).length)) {
+    DateDD.classList.remove("activeFlex");
+    ExperienceTab1.classList.remove("showLocationBTN");
+    pinkSearchDD.classList.remove("searchShowDD");
+
+  }
+  if ((!$(event.target).closest(ExperienceTab2).length) && (!$(event.target).closest(ExperienceTab2).length)) {
+    pinkSearchDD.classList.remove("searchShowDDD");
+    ExperienceTab2.classList.remove("showDateBTN");
+  }
   // for signin
   if ((!$(event.target).closest(userdd).length) && (!$(event.target).closest(userddBTN).length)) {
     userdd.classList.remove("show")
@@ -88,18 +116,19 @@ $(document).click(function (event) {
     locationBTN.classList.remove("showLocationBTN");
     checkinn.classList.remove("checkinSEC");
     checkoutt.classList.remove("checkoutSEC");
-    pinkSearch.classList.remove("serachShow");
+    pinkSearch.classList.remove("searchShow");
   }
   // for guests
   if ((!$(event.target).closest(guedtsDD).length) && (!$(event.target).closest(guestsBTN).length)) {
     guestsBTN.classList.remove("showGuestBTN");
-    pinkSearch.classList.remove("serachShowG");
+    pinkSearch.classList.remove("searchShowG");
     guedtsDD.classList.remove("guedtsDDshow");
     checkoutt.classList.remove("checkoutSECBRD");
     checkinn.classList.remove("checkinG");
     checkoutt.classList.remove("checkoutG");
     locationBTN.classList.remove("locationG");
   }
+
 });
 
 
@@ -116,7 +145,7 @@ incChild.addEventListener('click', function (event) {
     countervalueC += 1;
     childCoun.innerHTML = countervalueC;
     incChild.classList.remove("forbiddenIcon");
-  }else{
+  } else {
     incChild.classList.add("forbiddenIcon");
   }
 });
@@ -126,7 +155,7 @@ decChild.addEventListener('click', function (event) {
     countervalueC -= 1;
     childCoun.innerHTML = countervalueC;
     decChild.classList.remove("forbiddenIcon");
-  }else{
+  } else {
     decChild.classList.add("forbiddenIcon");
   }
 });
@@ -144,7 +173,7 @@ incInfants.addEventListener('click', function (event) {
     countervalueI += 1;
     InfantsNum.innerHTML = countervalueI;
     incInfants.classList.remove("forbiddenIcon");
-  }else{
+  } else {
     incInfants.classList.add("forbiddenIcon");
   }
 });
@@ -154,7 +183,7 @@ decInfants.addEventListener('click', function (event) {
     countervalueI -= 1;
     InfantsNum.innerHTML = countervalueI;
     decInfants.classList.remove("forbiddenIcon");
-  }else{
+  } else {
     decInfants.classList.add("forbiddenIcon");
   }
 });
@@ -172,7 +201,7 @@ incAdualt.addEventListener('click', function (event) {
     countervalueA += 1;
     adualtCoun.innerHTML = countervalueA;
     incAdualt.classList.remove("forbiddenIcon");
-  }else{
+  } else {
     incAdualt.classList.add("forbiddenIcon");
   }
 });
@@ -182,7 +211,7 @@ decAdualt.addEventListener('click', function (event) {
     countervalueA -= 1;
     adualtCoun.innerHTML = countervalueA;
     decAdualt.classList.remove("forbiddenIcon");
-  }else{
+  } else {
     decAdualt.classList.add("forbiddenIcon");
   }
 });
